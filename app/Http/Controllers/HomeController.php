ee9010945c33c76b78b7e23f65196945d4a8112a
unknown <nyangi.admin@LT-0104.ccbrt.or.tz>
2018-04-11 15:35:10 +0300

    Initial Commit with SQLServer and ODBC Driver set

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Http\Controllers;
+use Auth;
+use Illuminate\Support\Facades\Request;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Http\RedirectResponse;
+use DB;
+use Session;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
+class HomeController extends Controller
+{
+	public function getHomepage(){
+		try {
+		    DB::connection()->getPdo();
+		} catch (\Exception $e) {
+		    die("Could not connect to the database.  Please check your configuration.");
+		}
+		return view('welcome');
+	}
+
+}
